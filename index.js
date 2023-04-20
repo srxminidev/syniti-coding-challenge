66b108686f3a82e6d5674c9024f4a7254c779aaa
srxminidev <125895166+srxminidev@users.noreply.github.com>
2023-04-20 10:41:22 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,24 +2,24 @@
 import './style.css';
 import data from './data.json';
 
-const findGoodData = () => {
+const getInvalidIds = () => {
   const seenRow = {};
   const filteredResult = data.filter((row) => {
-    const key = `${row.name}#${row.address}#${row.zip}`;
+    const key = `${row.name}_${row.address}_${row.zip}`;
     if (
       seenRow[key] ||
       !row.name ||
       !row.address ||
       !(row.zip && /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(row.zip))
     ) {
-      return false;
+      return true;
     }
     seenRow[key] = true;
-    return true;
+    return false;
   });
-  return filteredResult.map((row) => `<p1 class="row">${row.id}</p1>`);
+  return filteredResult.map((row) => `<p>${row.id}</p>`).join('');
 };
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = findGoodData();
+appDiv.innerHTML = getInvalidIds();
