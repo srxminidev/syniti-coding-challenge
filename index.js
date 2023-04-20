5cd6c3ae9a07e64e0e3bc1f67092bb8d6af403d8
srxminidev <125895166+srxminidev@users.noreply.github.com>
2023-04-20 08:07:14 +0530

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 // Import stylesheets
 import './style.css';
+import data from './data.json';
+
+const findGoodData = () => {
+  const seenRow = {};
+  const filteredResult = data.filter((row) => {
+    const key = `${row.name}#${row.address}#${row.zip}`;
+    if (
+      seenRow[key] ||
+      !row.name ||
+      !row.address ||
+      !(row.zip && /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(row.zip))
+    ) {
+      return false;
+    }
+    seenRow[key] = true;
+    return true;
+  });
+  return filteredResult.map((row) => `<p1 class="row">${row.id}</p1>`);
+};
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = findGoodData();
